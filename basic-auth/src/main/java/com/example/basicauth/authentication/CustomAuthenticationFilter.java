6bcc35ef24338fae5efc03ba31d17e5d1bdd7f29
daily1313 <kimsb7218@naver.com>
2024-10-03 19:25:23 +0900

    refactor: filter authentication filter logic

--- basic-auth/src/main/java/com/example/basicauth/authentication/CustomAuthenticationFilter.java
+++ basic-auth/src/main/java/com/example/basicauth/authentication/CustomAuthenticationFilter.java
@@ -1,33 +1,72 @@
 package com.example.basicauth.authentication;
 
+import com.example.basicauth.service.dto.LoginRequest;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
-import org.springframework.security.web.authentication.AbstractAuthenticationProcessingFilter;
-import org.springframework.security.web.util.matcher.RequestMatcher;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
-public class CustomAuthenticationFilter extends AbstractAuthenticationProcessingFilter {
+public class CustomAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
 
-    public CustomAuthenticationFilter(final RequestMatcher requestMatcher) {
-        super(requestMatcher);
+    public CustomAuthenticationFilter(
+            AuthenticationManager authenticationManager) {
+        super(authenticationManager);
     }
 
     @Override
-    public Authentication attemptAuthentication(final HttpServletRequest request, final HttpServletResponse response)
-            throws AuthenticationException, IOException, ServletException {
-        String username = request.getParameter("username");
-        String password = request.getParameter("password");
+    protected void successfulAuthentication(final HttpServletRequest request,
+                                            final HttpServletResponse response,
+                                            final FilterChain chain,
+                                            final Authentication authResult) throws IOException, ServletException {
+        SecurityContextHolder.getContext().setAuthentication(authResult);
 
-        if(username == null || password == null) {
-            throw new IllegalArgumentException();
+        response.setStatus(HttpServletResponse.SC_OK);
+        response.getWriter().write("Authentication successful");
+    }
+
+    @Override
+    protected void unsuccessfulAuthentication(final HttpServletRequest request,
+                                              final HttpServletResponse response,
+                                              final AuthenticationException failed) throws IOException, ServletException {
+        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+        response.getWriter().write("Authentication failed: " + failed.getMessage());
+    }
+
+    @Override
+    public Authentication attemptAuthentication(final HttpServletRequest request,
+                                                final HttpServletResponse response) {
+
+        UsernamePasswordAuthenticationToken token = null;
+
+        try {
+            token = getAuthRequest(request);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
         }
-
-        UsernamePasswordAuthenticationToken token = new UsernamePasswordAuthenticationToken(username, password);
 
         return getAuthenticationManager().authenticate(token);
     }
+
+    public UsernamePasswordAuthenticationToken getAuthRequest(final HttpServletRequest request) throws Exception {
+        try {
+            ObjectMapper objectMapper = new ObjectMapper();
+            LoginRequest loginRequest = objectMapper.readValue(request.getInputStream(), LoginRequest.class);
+            return new UsernamePasswordAuthenticationToken(loginRequest.username(), loginRequest.password());
+        } catch(UsernameNotFoundException e) {
+            throw new UsernameNotFoundException(e.getMessage());
+        } catch (Exception e) {
+            throw new Exception(e.getMessage(), e.getCause());
+        }
+    }
+
+
 }
