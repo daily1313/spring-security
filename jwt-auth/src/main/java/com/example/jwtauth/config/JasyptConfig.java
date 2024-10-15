e4c22345b85a1217fd7984ab6f10bf629939fda4
daily1313 <kimsb7218@naver.com>
2024-10-15 23:21:57 +0900

    chore: modify JasyptConfig

--- jwt-auth/src/main/java/com/example/jwtauth/config/JasyptConfig.java
+++ jwt-auth/src/main/java/com/example/jwtauth/config/JasyptConfig.java
@@ -1,5 +1,6 @@
 package com.example.jwtauth.config;
 
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
 import org.jasypt.encryption.StringEncryptor;
 import org.jasypt.encryption.pbe.PooledPBEStringEncryptor;
 import org.jasypt.encryption.pbe.config.SimpleStringPBEConfig;
@@ -7,22 +8,24 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+@EnableEncryptableProperties
 @Configuration
 public class JasyptConfig {
 
     @Value("${jasypt.encryptor.password}")
-    private String jasyptKey;
+    private String ENCRYPT_KEY;
 
     @Bean("jasyptStringEncryptor")
     public StringEncryptor stringEncryptor(){
         PooledPBEStringEncryptor encryptor = new PooledPBEStringEncryptor();
         SimpleStringPBEConfig config = new SimpleStringPBEConfig();
-        config.setPassword(jasyptKey);
+        config.setPassword(ENCRYPT_KEY);
         config.setPoolSize("1");
         config.setAlgorithm("PBEWithMD5AndDES");
         config.setStringOutputType("base64");
         config.setKeyObtentionIterations("1000");
         config.setSaltGeneratorClassName("org.jasypt.salt.RandomSaltGenerator");
+        config.setIvGeneratorClassName("org.jasypt.iv.NoIvGenerator");
         encryptor.setConfig(config);
         return encryptor;
     }
