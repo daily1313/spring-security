7af5b4ba2873abf5e6d7c2131bcee78596fe027b
daily1313 <kimsb7218@naver.com>
2024-11-03 16:36:41 +0900

    feat: add member entity

---
+++ jwt-auth/src/main/java/com/example/jwtauth/domain/Gender.java
@@ -0,0 +1,32 @@
+package com.example.jwtauth.domain;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonValue;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum Gender {
+
+    MAN("M"),
+    WOMAN("W");
+
+    private String value;
+
+    @JsonValue
+    public String getValue() {
+        return value;
+    }
+
+    @JsonCreator(mode = JsonCreator.Mode.DELEGATING)
+    public static Gender fromValue(String value) {
+        for (Gender gender : Gender.values()) {
+            if (gender.value.equalsIgnoreCase(value)) {
+                return gender;
+            }
+        }
+
+        throw new IllegalArgumentException();
+    }
+}
