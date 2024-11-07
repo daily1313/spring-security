97638a398d4433f806e1813ae79271ebdee51b55
daily1313 <kimsb7218@naver.com>
2024-11-08 00:45:07 +0900

    refactor: add exception class in handler

--- jwt-auth/src/main/java/com/example/jwtauth/handler/GlobalExceptionHandler.java
+++ jwt-auth/src/main/java/com/example/jwtauth/handler/GlobalExceptionHandler.java
@@ -1,5 +1,7 @@
 package com.example.jwtauth.handler;
 
+import com.example.jwtauth.handler.exception.ExpiredTokenException;
+import com.example.jwtauth.handler.exception.InvalidRefreshTokenException;
 import com.example.jwtauth.handler.exception.JwtAuthenticationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -9,11 +11,10 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
-    @ExceptionHandler(JwtAuthenticationException.class)
-    public ResponseEntity<ErrorResponse> handleInvalidJwtException(JwtAuthenticationException e) {
+    @ExceptionHandler({JwtAuthenticationException.class, InvalidRefreshTokenException.class, ExpiredTokenException.class})
+    public ResponseEntity<ErrorResponse> handleInvalidJwtException(RuntimeException e) {
         ErrorResponse errorResponse = ErrorResponse.of(HttpStatus.UNAUTHORIZED, e.getMessage());
-        return ResponseEntity
-                .status(HttpStatus.UNAUTHORIZED)
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
                 .body(errorResponse);
     }
 }
