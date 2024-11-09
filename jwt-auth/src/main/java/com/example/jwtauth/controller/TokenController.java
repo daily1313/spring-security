cc31fc96b3f38d66f387aada4ea5a67e651d8d18
daily1313 <kimsb7218@naver.com>
2024-11-10 00:05:34 +0900

    refactor: change annotation order

--- jwt-auth/src/main/java/com/example/jwtauth/controller/TokenController.java
+++ jwt-auth/src/main/java/com/example/jwtauth/controller/TokenController.java
@@ -11,9 +11,9 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
 @RequestMapping("/api/v2/auth")
 @RequiredArgsConstructor
+@RestController
 public class TokenController {
 
     private final TokenService tokenService;
